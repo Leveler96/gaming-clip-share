021ab6d5a2193bb3c35253a3a2ae33e4739c548d
Leveler96 <leveler96@gmail.com>
2025-03-18 16:58:02 -0400

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,14 @@
+const config = {
+  firebase: {
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id"
+  },
+  cloudinary: {
+    cloudName: "YOUR_CLOUD_NAME",
+    uploadPreset: "YOUR_UPLOAD_PRESET"
+  }
+};
