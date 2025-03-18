a162784d12e56665973a5813e99eb60d52b077e0
Leveler96 <leveler96@gmail.com>
2025-03-18 16:50:24 -0400

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,14 @@
+const config = {
+  firebase: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "zenkai-share.firebaseapp.com",
+    projectId: "zenkai-share",
+    storageBucket: "zenkai-share.appspot.com",
+    messagingSenderId: "142738616429",
+    appId: "1:142738616429:web:2c84c2d631ff1b9e884f64"
+  },
+  cloudinary: {
+    cloudName: "dj2v5reza",
+    uploadPreset: "gaming_clips"
+  }
+};
